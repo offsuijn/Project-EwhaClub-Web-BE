320adeb4b87610cad9582e5c5f65efc9c466e76e
offsujin <offsujin@gmail.com>
2022-05-27 19:51:21 +0900

    Add Scan config

--- src/main/java/com/gdscewha/ewhaclub/EwhaclubApplication.java
+++ src/main/java/com/gdscewha/ewhaclub/EwhaclubApplication.java
@@ -2,8 +2,12 @@ package com.gdscewha.ewhaclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.gdscewha.ewhaclub.repository"})
+@EntityScan(basePackages = {"com.gdscewha.ewhaclub"})
 public class EwhaclubApplication {
 
 	public static void main(String[] args) {
