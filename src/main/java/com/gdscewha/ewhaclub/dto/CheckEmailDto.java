61686e568a709f19959f77db17385bd3946943b8
offsujin <offsujin@gmail.com>
2022-06-17 02:08:00 +0900

    Add Constructors

--- src/main/java/com/gdscewha/ewhaclub/dto/CheckEmailDto.java
+++ src/main/java/com/gdscewha/ewhaclub/dto/CheckEmailDto.java
@@ -1,13 +1,17 @@
 package com.gdscewha.ewhaclub.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CheckEmailDto {
 
     @NotBlank
