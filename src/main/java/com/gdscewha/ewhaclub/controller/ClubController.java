e12d603c858981af3b20376b34469f984b76cc2f
offsujin <offsujin@gmail.com>
2022-06-20 00:30:32 +0900

    Add @CrossOrigin annotation

--- src/main/java/com/gdscewha/ewhaclub/controller/ClubController.java
+++ src/main/java/com/gdscewha/ewhaclub/controller/ClubController.java
@@ -10,14 +10,12 @@ import com.gdscewha.ewhaclub.service.ClubService;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class ClubController {
--- src/main/java/com/gdscewha/ewhaclub/controller/UserController.java
+++ src/main/java/com/gdscewha/ewhaclub/controller/UserController.java
@@ -7,13 +7,11 @@ import com.gdscewha.ewhaclub.dto.UserDto;
 import com.gdscewha.ewhaclub.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value="/users")
